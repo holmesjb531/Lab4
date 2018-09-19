80c095384a9b16f9a3e9b86e6f6400e2fdfac348
holmesjb531 <jaymoney53198@gmail.com>
2018-09-19 14:20:09 -0400

    Lab4

---
+++ Lab4.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+
+public class Lab4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] labArray = new int[5];
+		
+		
+
+		
+		
+	}
+	
+	public static Integer[] sortArray(Integer[] labInt) {
+		
+		Scanner in = new(System.in);
+		
+		
+		//make a copy of labInt
+		
+		
+		for(int i = 1; i < labArray.length; i++) {
+			
+		}
+			
+		//sort the copy
+		//reverse the elements in the sorted copy
+		//return the reversed array
+		return null;
+	}
+
+}
